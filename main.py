b9371c5263e32aee720668e98c4d7cbd6345a40e
solidguy7 <kirill15grishkov01@gmail.com>
2023-03-23 21:07:59 +0300

    Fixed

--- main.py
+++ main.py
@@ -2,44 +2,44 @@ def validate(data):
     ...
 
 def flat(errors: dict) -> dict:
-    out = {}
-    def inner(obj, keys="[", value="]["):
-        if type(obj) is dict:
-            for k in obj:
-                inner(obj[k], keys + repr(k) + value)
-        elif type(obj) is list:
-            for num, item in enumerate(obj):
-                inner(item, keys + repr(num) + value)
+    result = {}
+    def inner(errors, key=[]):
+        if type(errors) is dict:
+            for k, v in errors.items():
+                inner(v, key + [k])
+        elif type(errors) is list:
+            for obj in errors:
+                inner(obj, key)
         else:
-            out.update({keys[:-1]: obj})
+            result.update({'.'.join(key): errors})
     inner(errors)
-    return out
+    return result
 
 # допустим мы уже преобразовали json в python словарь json.loads
 errors = {
     "last_name": "Имя должно состоять из букв",
-        "birth_place": {
-            "address": {
-                "parts": [
-                    {
-                        "0": {
-                            "id": "Неверный идентификатор",
-                        },
+    "birth_place": {
+        "address": {
+            "parts": [
+                {
+                    "0": {
+                        "id": "Неверный идентификатор",
                     },
-                    {
-                        "1": {
-                            "id": "Неверный идентификатор",
-                        },
+                },
+                {
+                    "1": {
+                        "id": "Неверный идентификатор",
                     },
-                ],
-            },
+                },
+            ],
         },
-        "groups": [
-            {
-                "1": "Группа workers не существует",
-            },
-        ],
-    }
+    },
+    "groups": [
+        {
+            "1": "Группа workers не существует",
+        },
+    ],
+}
 
 flat_errors = flat(errors)
 print(flat_errors)
